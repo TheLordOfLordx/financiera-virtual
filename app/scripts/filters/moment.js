d413100a2fb15eb493d549024ac63debb48caa9e
Armitage <listerine1989@gmail.com>
2017-07-18 13:43:02 -0500

    latest

--- app/scripts/filters/moment.js
+++ app/scripts/filters/moment.js
@@ -1,5 +1,5 @@
 angular.module('shoplyApp').filter('dateRange', function (){
-  return function (input, momentFn /*, param1, param2, ...param n */) {
+  return function (input, momentFn) {
     var args = Array.prototype.slice.call(arguments, 2),
         momentObj = moment(input);
     return momentObj[momentFn].apply(momentObj, args);
